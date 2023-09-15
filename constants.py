9097b364dbb62fec10f78a96c8690afb578945b6
Simon Egorov <rhinorofl@gmail.com>
2023-09-15 13:34:30 +0700

    Create a constant with endpoints

---
+++ constants.py
@@ -0,0 +1,16 @@
+DESTINATIONS = {
+    "files": [
+        {
+            "name": "file1.txt",
+            "endpoints": ["ftp", "folder"],
+        },
+        {
+            "name": "file2.txt",
+            "endpoints": ["owncloud", "ftp"],
+        },
+        {
+            "name": "file3.txt",
+            "endpoints": ["folder", "owncloud", "ftp"],
+        },
+    ]
+}
