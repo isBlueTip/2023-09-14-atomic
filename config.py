f1d0bec18d6d3aabcca02b5cf1d176d97f5036a5
Simon Egorov <rhinorofl@gmail.com>
2023-09-15 13:33:57 +0700

    Create config file

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+
+class Config:
+    FTP_ADDRESS = os.getenv("FTP_ADDRESS", "ftp://ftp.automiq.ru")
+    FTP_USER = os.getenv("FTP_USER", "xxxx-123321")
+    FTP_PASSWORD = os.getenv("FTP_PASSWORD", "_xxx_aaa11")
+
+    OWNCLOUD_URL = os.getenv("OWNCLOUD_URL", "https://fs.automiq.ru/owncloud/index.php/")
+    OWNCLOUD_PASSWORD = os.getenv("OWNCLOUD_PASSWORD", "_xxx_aaa11")
+
+    OWNCLOUD_FOLDER_PATH = os.getenv("OWNCLOUD_FOLDER_PATH", "/tmp/xxxx-123321/")
